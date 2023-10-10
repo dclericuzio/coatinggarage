03f9784bc3f604cb2478395e761bb303888a097c
= <dante.clericuzio@fintopia.tech>
2023-10-10 13:47:40 +0700

    first commit

---
+++ components/footer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function footer() {
+  return (
+    <div className='bg-red-100'>footer</div>
+  )
+}
---
+++ components/navigationbar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function navigationbar() {
+  return (
+    <div className='bg-blue-100'>navigationbar</div>
+  )
+}
